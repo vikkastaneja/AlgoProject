cbb766ffc31522e15a273db0f61cd49c5444dcfd
vikastaneja <vikas.vtaneja@gmail.com>
2014-02-12 00:39:23 -0800

    Adding third problem's solution in strings chapter in cracking the coding interview book

--- src/org/vikastaneja/crackingcodinginterviews/Chapter1.java
+++ src/org/vikastaneja/crackingcodinginterviews/Chapter1.java
@@ -2,6 +2,9 @@ package org.vikastaneja.crackingcodinginterviews;
 
 import com.google.common.base.Preconditions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * In this file, I will solve all the problems of chapter1
  * Created by vikastaneja on 2/10/14.
@@ -36,5 +39,61 @@ public class Chapter1 {
         }
 
         return true;
+    }
+
+    /**
+     * Problem: Given two string, find if one is permutation of other.
+     * Solution: Out of different solution, I have selected to implement it using hashmap.
+     * @param s1    First string
+     * @param s2    Second string
+     * @return  true if string are anagram, false otherwise
+     */
+    public static final Boolean isAnagram(final String s1, final String s2) {
+        Preconditions.checkNotNull(s1);
+        Preconditions.checkNotNull(s2);
+        int s1Length = s1.length();
+        int s2Length = s2.length();
+        if (s1Length != s2Length) {
+            return false;
+        }
+
+        if (s1Length == 0 && s2Length == 0) {
+            return true;
+        }
+
+        if ((s1Length == 0 && s2Length != 0) || (s2Length == 0 && s1Length != 0)) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s1Length; i++) {
+            Character ch = s1.charAt(i);
+            if (map.containsKey(ch)) {
+                Integer val = map.get(ch);
+                val++;
+                map.put(ch, val);
+            }
+            else {
+                map.put(ch, 1);
+            }
+        }
+
+        for (int i = 0; i < s2Length; i++) {
+            Character ch = s2.charAt(i);
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+
+            Integer val = map.get(ch);
+            val--;
+            if (val < 0) {
+                return false;
+            }
+
+            map.put(ch, val);
+        }
+
+        return true;
+
     }
 }
--- src/org/vikastaneja/crackingcodinginterviews/MainClass.java
+++ src/org/vikastaneja/crackingcodinginterviews/MainClass.java
@@ -5,9 +5,17 @@ package org.vikastaneja.crackingcodinginterviews;
  */
 public class MainClass {
     public static void main(String []args) {
+        System.out.println("**** First string problem ****");
         String noDuplicates = "LazyBrown";
         String duplicates = "Lazy Brown Fox";
         System.out.println(Chapter1.isDuplicatePresent(noDuplicates));
         System.out.println(Chapter1.isDuplicatePresent(duplicates));
+
+        System.out.println("**** Second string problem ****");
+        String str1 = "aaaabbbbcccc";
+        String str2 = "cbacbacbacba";
+        System.out.println(Chapter1.isAnagram(str1, str2));
+        System.out.println(Chapter1.isAnagram("", str2));
+
     }
 }

362e8d4b50e0f85c839add630c2802bff71f3bd7
vikastaneja <vikas.vtaneja@gmail.com>
2014-07-22 00:47:56 -0700

    Adding sections for scompany specific questions

---
+++ src/org/vikastaneja/companies/Expedia.java
@@ -0,0 +1,8 @@
+package org.vikastaneja.companies;
+
+/**
+ * Created by vikastaneja on 7/22/14.
+ * This class will have algorithm questions from http://www.careercup.com/page?pid=expedia-interview-questions
+ */
+public class Expedia {
+}
--- src/org/vikastaneja/companies/expedia.java
+++
@@ -1,8 +0,0 @@
-package org.vikastaneja.companies;
-
-/**
- * Created by vikastaneja on 7/21/14.
- * Questions to be picked from: http://www.careercup.com/page?pid=expedia-interview-questions
- */
-public class expedia {
-}

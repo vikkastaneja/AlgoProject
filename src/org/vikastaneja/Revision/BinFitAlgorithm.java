9aeead3805069b05adb56800e3d79fa22ebceed6
vikas.vtaneja <vikas.vtaneja@gmail.com>
2017-07-24 00:28:18 -0400

    Update javadoc for bin package algorithm

--- src/org/vikastaneja/Revision/BinFitAlgorithm.java
+++ src/org/vikastaneja/Revision/BinFitAlgorithm.java
@@ -4,6 +4,17 @@ package org.vikastaneja.Revision;
  * Created by vikastaneja on 8/8/14.
  */
 public class BinFitAlgorithm {
+  /**
+   * We want to fit n objects of different sizes into unlimited number of boxes each can have size 'k'
+   * There is a first fit algorithm - we pick each iteratively, and fit into box. If the box does not have enough space, we create another box and fit the item into it
+   * There is a possible another approach - we keep a data structure such as an int array where each one will keep current available capacity of the respective bin.
+   * For each item, we will parse and find if there is a bin that can accomodate the current one. If none, then we will add another bin. This algorithm is still not optimal as it will still iteratively find out and fit into first one.
+   * Another approach is an enahancement of this one, where we will parse the complete array and find out the max capacity where the current item can fit into.
+   **/
+  private static int binPackageAlgorithm(int[] jobsSize) {
+  
+  }
+  
     private static int binFitAlgo(int[] server, int[]jobs, int start, int end) {
         if (server == null || jobs == null)
             throw new NullPointerException("Either jobs or server is null");
